8763c4271ad7b0e056e90a2bd95e437f30eaaaca
Kaupo <kaupo.kuusalu@cgi.com>
2019-05-31 00:26:26 +0300

    salvestaModal

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-// var video = document.querySelector('video');
-// toggleHideElementById("video");
+var video = document.querySelector('video');
+toggleHideElementById("video");
+toggleHideElementById("btn-stop-recording");
 
-// function captureCamera(callback) {
-//     toggleHideElementById("video");
-//     navigator.mediaDevices.getUserMedia({
-//         audio: true,
-//         video: true
-//     }).then(function(camera) {
-//         callback(camera);
-//     }).catch(function(error) {
-//         alert('Unable to capture your camera. Please check console logs.');
-//         console.error(error);
-//     });
-// }
+function captureCamera(callback) {
+    toggleHideElementById("video");
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    }).then(function(camera) {
+        callback(camera);
+    }).catch(function(error) {
+        alert('Unable to capture your camera. Please check console logs.');
+        console.error(error);
+    });
+}
 
-// function stopRecordingCallback() {
-//     video.src = video.srcObject = null;
-//     video.muted = false;
-//     video.volume = 1;
-//     video.src = URL.createObjectURL(recorder.getBlob());
+function stopRecordingCallback() {
+    video.src = video.srcObject = null;
+    video.muted = false;
+    video.volume = 1;
+    video.src = URL.createObjectURL(recorder.getBlob());
 
-//     recorder.camera.stop();
-//     recorder.destroy();
-//     recorder = null;
-//     toggleHideElementById("video");
-// }
-// var recorder; // globally accessible
-// document.getElementById('btn-start-recording').onclick = function() {
-//     this.disabled = true;
-//     captureCamera(function(camera) {
-//         video.muted = true;
-//         video.volume = 0;
-//         video.srcObject = camera;
-//         recorder = RecordRTC(camera, {
-//             type: 'video'
-//         });
-//         recorder.startRecording();
-//         // release camera on stopRecording
-//         recorder.camera = camera;
-//         document.getElementById('btn-stop-recording').disabled = false;
-//     });
-// };
-// document.getElementById('btn-stop-recording').onclick = function() {
-//     this.disabled = true;
-//     recorder.stopRecording(stopRecordingCallback);
-// };
-
+    recorder.camera.stop();
+    recorder.destroy();
+    recorder = null;
+    toggleHideElementById("btn-stop-recording");
+}
+var recorder; // globally accessible
+document.getElementById('btn-start-recording').onclick = function() {
+    this.disabled = true;
+    captureCamera(function(camera) {
+        video.muted = true;
+        video.volume = 0;
+        video.srcObject = camera;
+        recorder = RecordRTC(camera, {
+            type: 'video',
+            canvas: {
+                width: 400,
+                height: 240
+            }
+        });
+        recorder.startRecording();
+        // release camera on stopRecording
+        recorder.camera = camera;
+    });
+    toggleHideElementById("btn-stop-recording");
+};
+document.getElementById('btn-stop-recording').onclick = function() {
+    this.disabled = true;
+    recorder.stopRecording(stopRecordingCallback);
+};
 
 function toggleHideElementById(id) {
     var x = document.getElementById(id);
